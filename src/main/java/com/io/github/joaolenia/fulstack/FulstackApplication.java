9a39af4dcb3992f0b4de3b4b6fdaf82e6a2b714d
Joao Pedro <joaopedrogolenia@gmail.com>
2024-10-22 14:48:15 -0300

    first commit

---
+++ src/main/java/com/io/github/joaolenia/fulstack/FulstackApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.joaolenia.fulstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/github/joaolenia/fulstack/FulstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.joaolenia.fulstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
